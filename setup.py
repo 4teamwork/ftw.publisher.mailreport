84417f989cdf8a6ff9a56b6e92e42c156f0c650c
Jonas Baumann <j.baumann@4teamwork.ch>
2010-08-19 15:35:02 +0000

    setup.py: using version.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0-dev'
+version = open('ftw/publisher/mailreport/version.txt').read().strip()
 maintainer = 'Jonas Baumann'
 
 setup(name='ftw.publisher.mailreport',
