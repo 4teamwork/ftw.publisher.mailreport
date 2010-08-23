2b1fdf229c831747a433f82738a428c607ffffff
Jonas Baumann <j.baumann@4teamwork.ch>
2010-08-23 10:28:40 +0000

    added viewlet, added tests for viewlet

---
+++ ftw/publisher/mailreport/viewlets/link.py
@@ -0,0 +1,10 @@
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.layout.viewlets import ViewletBase
+
+
+class MailreportConfigLink(ViewletBase):
+    """Viewlet for adding a link to the ftw.publisher.sender viewlet manager
+    which is shown in the publisher control panel.
+    """
+    
+    render = ViewPageTemplateFile('link.pt')
