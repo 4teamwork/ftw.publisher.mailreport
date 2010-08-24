907c389e6d1d223b2a4e845ca871f47d06831781
Jonas Baumann <j.baumann@4teamwork.ch>
2010-08-24 09:03:14 +0000

    fixed event handler

--- ftw/publisher/mailreport/handlers.py
+++ ftw/publisher/mailreport/handlers.py
@@ -2,10 +2,10 @@ from ftw.publisher.mailreport.interfaces import IReportNotifier
 from ftw.publisher.mailreport.utils import is_interval_expired
 
 
-def invoke_notification(event):
+def invoke_notification(obj, event):
     """Event handler for `IQueueExecutedEvent` which invokes the
     `IReportNotifier` adapter.
     
     """
     if is_interval_expired():
-        return IReportNotifier(event.object)()
+        return IReportNotifier(obj)()
