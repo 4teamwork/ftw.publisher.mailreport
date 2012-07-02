39ef4ace03e5709b9cc7519032186608dac42562
Jonas Baumann <jone@jone.ch>
2012-07-02 09:07:53 +0200

    Update package metadata.

--- setup.py
+++ setup.py
@@ -11,36 +11,46 @@ tests_require = [
 
 setup(name='ftw.publisher.mailreport',
       version=version,
-      description="Sends scheduled status report mails (for ftw.publisher)" +\
-          ' (Maintainer: %s)' % maintainer,
-      long_description=open("README.txt").read() + "\n" + \
-          open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      description='An ftw.publisher addon for sending scheduled '
+      'publishing report mails.',
+
+      long_description=open('README.txt').read() + '\n' +
+      open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
+        'Framework :: Plone',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
-      keywords='ftw.publisher mail report scheduled',
-      author='%s, 4teamwork GmbH' % maintainer,
+
+      keywords='ftw publisher mail report scheduled',
+      author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
-      url='http://psc.4teamwork.ch/4teamwork/ftw/ftw-publisher-mailreport',
+      url='https://github.com/4teamwork/ftw.publisher.mailreport',
+
       license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw', 'ftw.publisher'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
         'setuptools',
         'z3c.autoinclude',
         'ftw.publisher.sender',
         'plone.fieldsets',
-        # -*- Extra requirements: -*-
         ],
+
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
-      entry_points="""
+
+      entry_points='''
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
-      """,
+      ''',
       )
