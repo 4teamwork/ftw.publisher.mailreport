1b78072e5dedf6e6d7658e4cd35697479d0dcf27
Jonas Baumann <jone@jone.ch>
2012-07-02 09:09:20 +0200

    Move version to setup.py, set target version to 2.0b1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = open('ftw/publisher/mailreport/version.txt').read().strip()
+version = '2.0b1.dev0'
 maintainer = 'Jonas Baumann'
 
 tests_require = [
