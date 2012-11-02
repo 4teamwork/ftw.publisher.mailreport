494c89b121489e59bb144b4f51e2211cc1e3e894
Jonas Baumann <jone@jone.ch>
2012-11-02 14:44:21 +0100

    Remove old base.py (PloneTestCase)

--- ftw/publisher/mailreport/tests/base.py
+++
@@ -1,8 +0,0 @@
-from Products.PloneTestCase import ptc
-from ftw.publisher.mailreport.testing import mailreport_functional_layer
-
-
-class MailreportBasicTestCase(ptc.FunctionalTestCase):
-    """Base class for integration tests."""
-
-    layer = mailreport_functional_layer
